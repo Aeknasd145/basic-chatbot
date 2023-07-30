464fbcdab969e66aac6ad011197b0710272c8ffe
Abbas Eren Kılıç <67686692+Aeknasd145@users.noreply.github.com>
2023-07-31 00:00:03 +0300

    Create chatbot.php

---
+++ chatbot.php
@@ -0,0 +1,20 @@
+<?php 
+  $siteName = 'Web Site Name';
+  $siteSlogan = 'Web Site Slogan';
+  $placeHolder = 'Question...';
+  $errorMessage = 'Sorry, I couldn\'t understand your message, you can contact with us via <a href="mailto:info@site.com">info@site.com</a>';
+?>
+<div id="chatButton">?</div>
+<div id="chatContainer" class="chatContainer mh-100">
+	<div id="chatHeader" class="justify-content-between">
+		<div class="d-flex align-items-center">
+			<div>
+				<div id="siteName"><?php echo $siteName; ?></div>
+				<div id="siteSlogan"><?php echo $siteSlogan; ?></div>
+			</div>
+		</div>
+		<div id="closeChatBox" class="mr-3 font-weight-bold">X</div>
+	</div>
+	<div id="chatBox mh-100"></div>
+	<textarea type="text" rows="1" id="chatInput" placeholder="<?php echo $placeHolder; ?>" onkeydown="check(event, '<?php echo $errorMessage; ?>')"></textarea>
+</div>
